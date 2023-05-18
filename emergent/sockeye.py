837f34567d496c14db132108479edc5dcadb57b3
Kevin P Nebiolo, PhD <61742537+knebiolo@users.noreply.github.com>
2023-05-18 10:56:40 -0400

    solving problems
    developed solutions for Hz, efficient raster search with rasterio, and an algorithm for the battery

--- emergent/sockeye.py
+++ emergent/sockeye.py
@@ -17,9 +17,11 @@ import os
 import geopandas as gpd
 import shapely
 import rasterio
+import numpy as np
+from scipy import interpolate
     
 # create a sockeye agent 
-def sockeye():
+class sockeye():
     ''' Python class object for a sockeye agent. 
     
     Class object contains all of the sockeye's attributes, while the methods
@@ -40,6 +42,21 @@ def sockeye():
             body depth = mm
             velocity = cms SOG'''
             
+        # initialization methods
+        def sex(self, basin):
+            '''function simulates a sex for a given basin'''
+            
+        def length(self, basin):
+            '''function simulates a fish length out of the user provided basin and 
+            sex of fish'''
+            
+        def weight(self, basin):
+            '''function simulates a fish weight out of the user provided basin and 
+            sex of fish'''
+            
+        def body_depth(self, basin):
+            '''function simulates a fish body depth out of the user provided basin and 
+            sex of fish'''    
         # initialize morphometric paramters
         self.ID = ID
         self.sex = sex(basin)
@@ -66,31 +83,89 @@ def sockeye():
         self.pos = (0.,0.,0.)
         
         # create agent database and write agent parameters 
-        self.hdf = pd.HDFStore(os.path.join(outputWS,'%s.h5'%('agent_%s.h5'%(ID))))
+        self.hdf = pd.HDFStore(os.path.join(ws,'%s.h5'%('agent_%s.h5'%(ID))))
         self.hdf['agent'] = pd.DataFrame.from_dict({'ID':self.ID,
                                                     'sex':self.sex,
                                                     'length':self.length,
                                                     'weight':self.weight,
                                                     'body_depth':self.body_depth})
         self.hdf.flush()
+    
+    
+    def thrust (U,L,f):
+        '''Lighthill 1970 thrust equation. '''
+        # density of freshwater assumed to be 1
+        rho = 1.0 
         
-    # initialization methods
-    def sex(self, basin):
-        '''function simulates a sex for a given basin'''
+        # theta that produces cos(theta) = 0.85
+        theta = 32.
         
-    def length(self, basin):
-        '''function simulates a fish length out of the user provided basin and 
-        sex of fish'''
+        # sockeye parameters (Webb 1975, Table 20)
+        length_dat = np.array([5.,10.,15.,20.,25.,30.,40.,50.,60.])
+        speed_dat = np.array([37.4,58.,75.1,90.1,104.,116.,140.,161.,181.])
+        amp_dat = np.array([1.06,2.01,3.,4.02,4.91,5.64,6.78,7.67,8.4])
+        wave_dat = np.array([53.4361,82.863,107.2632,131.7,148.125,166.278,199.5652,230.0044,258.3])
+        edge_dat = np.array([1.,2.,3.,4.,5.,6.,8.,10.,12.])
         
-    def weight(self, basin):
-        '''function simulates a fish weight out of the user provided basin and 
-        sex of fish'''
+        # fit univariate spline
+        amplitude = interpolate.UnivariateSpline(length_dat,amp_dat,k = 2) 
+        wave = interpolate.UnivariateSpline(speed_dat,wave_dat,k = 1) 
+        trail = interpolate.UnivariateSpline(length_dat,edge_dat,k = 1) 
         
-    def body_depth(self, basin):
-        '''function simulates a fish body depth out of the user provided basin and 
-        sex of fish'''
+        # interpolate A, V, B
+        A = amplitude(L)
+        V = wave(U)
+        B = trail(L) 
         
-    # 
+        # Calculate thrust
+        m = (np.pi * rho * B**2)/4.
+        W = (f * A * np.pi)/1.414
+        w = W * (1 - U/V)
+            
+        thrust = m * W * w * U - (m * w**2 * U)/(2. * np.cos(np.radians(theta)))
+        
+        return (thrust)
+    
+    def frequency (U,L,D):
+        ''' Function for tailbeat frequency.  By setting Lighthill (1970) equations 
+        equal to drag, we can solve for tailbeat frequency (Hz).  
+        
+        Density of water (rho) is assumed to be 1
+        
+        Input parameters for this function include:
+            U = speed over ground (or swim speed?) (cm/s)
+            _lambda = length of the propulsive wave
+            L = length, converted to trailing edge span (cm) = 0.2L
+            D = force of drag'''
+            
+        # density of freshwater assumed to be 1
+        rho = 1.0 
+        
+        # theta that produces cos(theta) = 0.85
+        theta = 32.
+        
+        # sockeye parameters (Webb 1975, Table 20)
+        length_dat = np.array([5.,10.,15.,20.,25.,30.,40.,50.,60.])
+        speed_dat = np.array([37.4,58.,75.1,90.1,104.,116.,140.,161.,181.])
+        amp_dat = np.array([1.06,2.01,3.,4.02,4.91,5.64,6.78,7.67,8.4])
+        wave_dat = np.array([53.4361,82.863,107.2632,131.7,148.125,166.278,199.5652,230.0044,258.3])
+        edge_dat = np.array([1.,2.,3.,4.,5.,6.,8.,10.,12.])
+        
+        # fit univariate spline
+        amplitude = interpolate.UnivariateSpline(length_dat,amp_dat,k = 2) 
+        wave = interpolate.UnivariateSpline(speed_dat,wave_dat,k = 1) 
+        trail = interpolate.UnivariateSpline(length_dat,edge_dat,k = 1) 
+        
+        # interpolate A, V, B
+        A = amplitude(L)
+        V = wave(U)
+        B = trail(L)    
+        
+        # now that we have all variables, solve for f
+        #sol1 = -1 * np.sqrt(D*V**2*np.cos(np.radians(theta))/(A**2*B**2*U*np.pi**3*rho*(U - V)*(-0.062518880701972*U - 0.125037761403944*V*np.cos(np.radians(theta)) + 0.062518880701972*V)))
+        Hz = np.sqrt(D*V**2*np.cos(np.radians(theta))/(A**2*B**2*U*np.pi**3*rho*(U - V)*(-0.062518880701972*U - 0.125037761403944*V*np.cos(np.radians(theta)) + 0.062518880701972*V)))
+        
+        return Hz
 
 
     
---
+++ scripts/fatigue.py
@@ -0,0 +1,245 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 16 19:36:35 2023
+
+@author: KNebiolo
+
+Script Intent: Create a battery based on models of Castro-Santos and Brett.
+
+The fatigue method provides a means to calculate the fatigue level of a fish.   
+
+The work of Castro-Santos (2005) gives us fatigue time as a function of swimming 
+speed and fish length.  
+
+ln(T) = a + b Us
+
+where T = time in seconds, a and b are coefficients, and Us is swim speed in 
+body lengths per second. 
+
+In sustained swimming mode, fish will not fatigue. However, once a threshold is 
+passed and a fish is in prolonged swimming mode, it is swimming anaerobically 
+and utilizing energy reserves.  As fish swim even faster still, they will enter 
+burst mode, which comes at an even higher metabolic cost.   
+
+When fish start swimming anaerobically, their energy reserves start to decrease.  
+At the end of a timestep, we reduce energy reserves proportional to the length 
+of the timestep compared to the time until fatigue. For example, say a fish has 
+45 seconds until it fatigues at its current swim speed, and the timestep is 0.25 
+seconds.  At the end of the first timestep (0.25 seconds later) the fish has 
+44.75 seconds until it fatigues and 99.44% of its original energy reserves.  
+At the end of the next time step, if it does not change swimming speed, the fish 
+has 44.5 seconds until it fatigues.  However, the fish may need to accelerate 
+to maintain speed over ground.  If it accelerates and increases swim speed we 
+may find that there is now 40 seconds until the fish fatigues.  But, the fish 
+does not have 100% of the necessary reserves, it has 99.44%.  Therefore, the 
+actual time to fatigue is 39.776 seconds (40 * 0.9944). 
+
+When a fish reduces swim speed below anaerobic levels, it starts to recover.  
+See Figure 18 (Brett 1964).  The fish does not reach 100% recovered until 11
+hours later.  However, studies show that the fish can perform at high levels 
+again with as little as 45 minutes between tests.  Brett even considers fish 
+recovered after 3.2 hours because they return to spontaneous activity.  
+Maybe a fish must wait at least 45 minutes or up until 3.2 hours?  At 0 minutes 
+0 % recovered, 45 minutes 100% recovered, can we fit exponential decay to that? 
+"""
+# import dependencies
+import numpy as np
+import os
+import pandas as pd
+from scipy import interpolate
+import matplotlib.pyplot as plt
+
+# identify workspaces
+inputWS = r"J:\2819\005\Calcs\ABM\Data"
+outputWS = r"J:\2819\005\Calcs\ABM\Output"
+
+
+# import recovery data and fit function 
+recover = pd.read_csv(os.path.join(inputWS,'recovery.csv'))
+recover['Seconds'] = recover.Minutes * 60.
+recovery = interpolate.CubicSpline(recover.Seconds,recover.Recovery,extrapolate = True,)
+secs = np.arange(0,2760,60) 
+int_recover = recovery(secs)
+# plt.plot(secs,int_recover)
+# plt.plot(recover.Seconds,recover.Recovery,'ro')
+# plt.show
+
+
+# Identify Variables Used in Battery Function 
+'''variables are exogenous and will be properties of an agent class object,
+therefore instead of passing all variables we will just pass self'''
+
+# use American Shad as surrogate for now
+max_U = 7.2
+a_p = 10.7
+b_p = -1.0
+a_s = 6.16
+b_s = -0.33
+
+# swimming mode
+mode = 'sustained' # sustained, prolonged, sprint, or holding 
+
+# current and previous swim speed - if swim speed changes so does rate of fatigue
+swim_speed = 2. 
+
+# current battery level
+batt = 1.
+
+# recharge state - initial battery drain has recharge state of 1.
+recharge = 1.
+
+# running time to fatigue in seconds
+ttfr = 0.0
+
+# change in time per time step in seconds
+dt = 1.
+
+# total recovery time
+stopwatch = 0.0
+
+# prolonged time
+prolong_time = dt
+
+# create some arrays to hold data for QC
+ttf_arr = np.zeros(3600)
+stopwatch_arr = np.zeros(3600)
+batt_arr = np.zeros(3600)
+mode_arr = np.repeat('',3600)
+
+for t in np.arange(0,3600,1):
+    # fish is moving and behavior may or may not be aneorbic
+    if batt > 0.1 and mode != 'holding':
+        # # make things interesting, throw a random swim speed
+        swim_speed = np.random.randint(1,10,1)[0]
+        
+        # calculate ttf
+        if 4 < swim_speed <= max_U:
+            # reset stopwatch
+            stopwatch = 0.0
+            
+            # set swimming mode
+            mode = 'prolonged'
+            
+            # calculate time to fatigue at current swim speed
+            ttf = np.exp(a_p + swim_speed * b_p)
+            
+        elif swim_speed > max_U:
+            # reset stopwatch
+            stopwatch = 0.0
+            
+            # set swimming mode
+            mode = 'sprint'
+            
+            # calculate time to fatigue at current swim speed
+            ttf = np.exp(a_s + swim_speed * b_s)
+            
+        else:
+            mode = 'sustained'
+            
+            # calculate recovery % at beginning of time step
+            rec0 = recovery(stopwatch) / 100.
+            
+            # make sure realistic value - also can't divide by 0
+            if rec0 < 0.0:
+                rec0 = 0.0
+                
+            # calculate recovery % at end of time step
+            rec1 = recovery(stopwatch + dt) / 100. 
+            
+            if rec1 > 1.0:
+                rec1 = 1.0
+            
+            # calculate percent increase
+            per_rec = rec1 - rec0
+            
+            # stopwatch
+            stopwatch = stopwatch + dt
+            
+            ttf = 1
+            
+        # take into account the time that has already past - that's now how long a fish has
+        ttf0 = ttf - ttfr
+        
+        # calculate time to fatigue at end of time step
+        ttf1 = ttf0 - dt
+        
+        # add to running timer
+        ttfr = ttfr + dt
+        
+        if mode != 'sustained':
+            # calculate battery level
+            batt = recharge * ttf1/ttf
+            
+            # make sure battery drain is reasonable
+            if batt < 0.0:
+                batt = 0.0
+        else:
+            # calculate battery level
+            batt = batt + per_rec    
+            
+            # make sure battery recharge is reasonable
+            if batt > 1.0:
+                batt = 1.0
+        
+        # # increase swim speed
+        # swim_speed = swim_speed * 1.01
+        
+        if batt <= 0.1:
+            mode = 'holding'
+            ttfr = 0.0
+            
+    # fish is station holding and recovering    
+    else:
+        ''' recovery is based on how long a fish has been in a recovery state'''
+        # calculate recovery % at beginning of time step
+        rec0 = recovery(stopwatch) / 100.
+        
+        # make sure realistic value - also can't divide by 0
+        if rec0 < 0.0:
+            rec0 = 0.0
+            
+        # calculate recovery % at end of time step
+        rec1 = recovery(stopwatch + dt) / 100. 
+        
+        if rec1 > 1.0:
+            rec1 = 1.0
+        
+        # calculate percent increase
+        per_rec = rec1 - rec0
+        
+        # calculate battery level
+        batt = batt + per_rec
+        
+        # stopwatch
+        stopwatch = stopwatch + dt
+        
+        # battery is charged enough to start moving again
+        if batt >= 0.9:
+            # reset recharge stopwatch
+            stopwatch = 0.0
+            
+            # change swimming mode to sustained
+            mode = 'sustained'
+            
+            # set the recharge rate to the current battery 
+            recharge = batt
+            
+            # set swim speed to something reasonable
+            swim_speed = 3.
+ 
+    # insert into array
+    batt_arr[t] = batt
+    ttf_arr[t] = ttfr
+    
+plt.plot(np.arange(0,3600,1),batt_arr)
+plt.show
+
+
+
+
+    
+    
+    
+    
+        
+    
